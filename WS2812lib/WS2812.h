dda799fc91162847f349085e89a92670b6cd3569
valerionew <valerionappi991@gmail.com>
2022-09-04 20:17:02 +0200

    simplified library

--- WS2812lib/WS2812.h
+++ WS2812lib/WS2812.h
@@ -19,45 +19,41 @@ namespace WSCONST{
 template<const int LENGTH>
 class WS2812{
   private:
-    std::array<RGB_t<uint8_t>, LENGTH> pixels;
     volatile uint8_t bitsbuffer[LENGTH * WSCONST::BITS_PER_LED + 1 + WSCONST::RESET_LENGTH]; // we use 1 byte per bit
 
   // Object for SPI communication  
     std::function<void(uint8_t*, uint16_t)> spi_transfer;
 
+
   public:
     WS2812(std::function<void(uint8_t*, uint16_t)> spi_transfer) : spi_transfer(spi_transfer){
       for (int i = 0; i < LENGTH; i++) {
-        pixels[i] = RGB_t<uint8_t>(0, 0, 0);
+        setPixel(i,(RGB_t<uint8_t>){0, 0, 0});
       }
     }
     void setPixel(const int index, const RGB_t<uint8_t> color) {
     	// it should really check that index is a valid index for the array
-      pixels[index] = color;
-    }
-    void show() {
-      uint8_t *buf = (uint8_t*) bitsbuffer; //first byte is zero to reset
-      
-      using namespace WSCONST;
-      for (int currentled = 0; currentled < LENGTH; currentled++){
 
-        for(int i = 0; i < 8; i++){
-          buf[currentled*24 + i + 0] = pixels[currentled].g & (1 << i) ? ONE_BITS : ZERO_BITS;
-          buf[currentled*24 + i + 8] = pixels[currentled].r & (1 << i) ? ONE_BITS : ZERO_BITS;
-          buf[currentled*24 + i +16] = pixels[currentled].b & (1 << i) ? ONE_BITS : ZERO_BITS;
-          }
+      using namespace WSCONST;
+
+      for(int i = 0; i < 8; i++){
+        bitsbuffer[index*24 + i + 0] = color.g & (1 << i) ? ONE_BITS : ZERO_BITS;
+        bitsbuffer[index*24 + i + 8] = color.r & (1 << i) ? ONE_BITS : ZERO_BITS;
+        bitsbuffer[index*24 + i +16] = color.b & (1 << i) ? ONE_BITS : ZERO_BITS;
       }
+    }
+    void show() {      
+      using namespace WSCONST;
+
       uint8_t zero = 0;
       spi_transfer(&zero, 1); // clear the bit first
       spi_transfer((uint8_t*)bitsbuffer, LENGTH * BITS_PER_LED + 1 + RESET_LENGTH);
 
     }
-    void setFrame(const std::array<RGB_t<uint8_t>, LENGTH> colors) {
-        pixels = colors;
-    }
+
     void clear() {
       for (int i = 0; i < LENGTH; i++) {
-        pixels[i] = RGB_t<uint8_t>(0, 0, 0);
+        setPixel(i,(RGB_t<uint8_t>){0, 0, 0});
       }
     }
 
