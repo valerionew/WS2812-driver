9098458109d7d9e7149cc9200688a5c368a7d185
valerionew <valerionappi991@gmail.com>
2022-09-10 17:03:39 +0200

    using now defaults copy and assign methods, removed problematic operators

--- WS2812lib/RGB.h
+++ WS2812lib/RGB.h
@@ -8,13 +8,10 @@ public:
   T r, g, b;
   RGB_t() : r(0), g(0), b(0) {}
   RGB_t(T r, T g, T b) : r(r), g(g), b(b) {}
-  RGB_t(const RGB_t& other) : r(other.r), g(other.g), b(other.b) {}
-  RGB_t& operator=(const RGB_t& other) {
-    r = other.r;
-    g = other.g;
-    b = other.b;
-    return *this;
-  }
+  
+  RGB_t(const RGB_t& other) = default;
+  RGB_t& operator=(const RGB_t& other) =  default;
+
   RGB_t& operator+=(const RGB_t& other) {
     r += other.r;
     g += other.g;
@@ -50,15 +47,6 @@ public:
     g /= scalar;
     b /= scalar;
     return *this;
-  }
-  RGB_t operator+(const RGB_t& other) const {
-    return RGB_t(*this) += other;
-  }
-  RGB_t operator-(const RGB_t& other) const {
-    return RGB_t(*this) -= other;
-  }
-  RGB_t operator*(const RGB_t& other) const {
-    return RGB_t(*this) *= other;
   }
 };
 
